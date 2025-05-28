7dff152e7aa3cebba61b84d6eaad32469aa59682
Samantha <contact@samanthanguyen.me>
2025-05-27 23:30:35 -0500

    feat: implement DateDurationSign() (#5)

--- tests/Record/DateDurationRecordTest.php
+++ tests/Record/DateDurationRecordTest.php
@@ -3,6 +3,7 @@
 namespace Temporal39\Tests\Record;
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Temporal39\Record\DateDurationRecord;
 
@@ -15,5 +16,21 @@ class DateDurationRecordTest extends TestCase {
 		$this->assertEqualsWithDelta( 0.0, $zero->months, 0.0 );
 		$this->assertEqualsWithDelta( 0.0, $zero->weeks, 0.0 );
 		$this->assertEqualsWithDelta( 0.0, $zero->days, 0.0 );
+	}
+
+	#[DataProvider( 'provideDateDurationSign' )]
+	public function testDateDurationSign(
+		DateDurationRecord $dateDuration,
+		int $expected,
+	): void {
+		$this->assertSame( $expected, $dateDuration->sign() );
+	}
+
+	public static function provideDateDurationSign(): array {
+		return [
+			[ new DateDurationRecord( 5.0, 4.0, 3.0, 2.0 ), 1 ],
+			[ new DateDurationRecord( -1, 4.0, 3.0, 2.0 ), -1 ],
+			[ new DateDurationRecord( 0.0, 0.0, 0.0, 0.0 ), 0 ],
+		];
 	}
 }
